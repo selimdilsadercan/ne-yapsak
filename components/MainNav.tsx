46869cf361ca9805c810e8b3f0e2878f10844fab
selimdilsadercanrtw <selim.ercan@remotetech.work>
2025-04-04 21:12:05 +0300

    husky

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Home, Compass, ListTodo, User, Users } from "lucide-react";
+import { Home, Compass, ListTodo, Users } from "lucide-react";
 
 const routes = [
   {
